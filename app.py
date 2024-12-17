5f30d4a34f672790215d99bbfef38053f5f0e7e8
Shu Zhi Lee <leeshuzhi47@gmail.com>
2024-12-17 14:07:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,5 +14,13 @@ def register():
 def login():
     return "<h1>Login Page</h1>"
 
+@app.route('/partner')
+def partner():
+    return "<h1>Become a Partner</h1>"
+
+@app.route('/about')
+def about():
+    return "<h1>About Us</h1>"
+
 if __name__ == '__main__':
     app.run(debug=True)
