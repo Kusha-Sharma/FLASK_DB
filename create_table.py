c872ca319dae0774a03d92e46a5fbaa9f9f58e98
kusha sharma <kusha.sharma@stud.uni-due.de>
2024-12-22 02:11:40 +0100

    created tables in db

--- create_table.py
+++ create_table.py
@@ -2,6 +2,11 @@ import sqlite3
 conn = sqlite3.connect('database.db')
 print('connected successfully to db')
 
-conn.execute('create table students(name TEXT, addr, TEXT )')
+conn.execute('create table if not exists Users(username TEXT, address TEXT, email TEXT, pincode TEXT, password TEXT, Telephone TEXT,current balance TEXT )')
+conn.execute('create table if not exists Restaurant(restaurantName TEXT, address TEXT, email TEXT, zipcode TEXT, Telephone TEXT, password TEXT)')
+conn.execute('create table if not exists Items(fooditems TEXT, description TEXT, price TEXT)')
+conn.execute('create table if not exists PaymentHistory( username TEXT, Restaurant TEXT, UserBalance TEXT, RestaurantBalance TEXT, Liferplatz TEXT)')
+conn.execute('create table if not exists Progress(RestaurantName TEXT, orderedItems TEXT, quantity TEXT, Status TEXT)')
+conn.execute('drop table students ')
 print('created table')
 conn.close()
