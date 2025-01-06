9146b2fac1c0101fa2bf96354f1067f253dee49f
kusha sharma <kusha.sharma@stud.uni-due.de>
2025-01-06 15:55:47 +0100

    commit

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import sqlite3
-import re, request, redirect, url_for, flash, session
-import sqlite3
+import re
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
