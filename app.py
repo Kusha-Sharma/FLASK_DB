1c58ea33aa49c9f55e82fcac534d2b48ead1e273
Shu Zhi Lee <leeshuzhi47@gmail.com>
2025-01-06 15:34:08 +0100

    Add initial files for Flask and related packages in virtual environment

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
-import sqlite3
-import re, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 import sqlite3
 from werkzeug.security import generate_password_hash, check_password_hash
 
